48b85b70537ce43b4e8801b972a49759470de1e9
MixMuffins <kag3372@rit.edu>
2013-11-26 14:23:13 -0500

    Finished up on the basic proof of concept build for the tool. Some error checking added.

--- README.md
+++ README.md
@@ -13,7 +13,10 @@ Usage
 Call/Run program using:<br />
 <code>./tessera</code> <br />
 
-After booting, Tessera will ask for a case name. Takes a string for input. <br />
+After booting, Tessera will ask if you'd like to make a case. <br />
+Creating a case means that all information entered will be stored in a file for easy record keeping. <br />
+Choosing no skips the case building option. <br />>
+Choosing yes brings you into the case building menu. Case takes a string for input. <br />
 <p><code>	Enter case name: 		Case#067 - Mark Phillips<br />
 </code> </p>
 Tessera will then ask for a list of investigator names. Terminate the list by entering a "." <br />
@@ -23,6 +26,8 @@ Tessera will then ask for a list of investigator names. Terminate the list by en
 </code></p>
 The next portion asked for is the location of the memory dump that is being analyzed; this location is verified by Tessera, so if Tessera can't find the image, it will ask for a new location.<br />
 <code>	Enter full path to image: 	/home/john/forensics/mark.img</code><br />
+
+Tessera then will hash the image using the SHA-1 algorithm. This hash will be displayed to you. If you chose to build a case, this information will be stored in a file at the end of the Tessera run. 
 
 Tessera then takes the image and runs imageinfo on it to get a list of profiles. It automatically lists these and then asks you to choose a number. If no number is specified (you just hit enter), it will grab the first option. If you choose OTHER, it will prompt for a profile name. This must be a valid profile that Volatility knows about. <br />
 <p><code>	Profile appears to be: <br />
@@ -35,19 +40,15 @@ Tessera then takes the image and runs imageinfo on it to get a list of profiles.
 Tessera will then attempt to built the case with the information that you have given it. If the case was successfully built and Tessera runs into no problems, it will inform the user of its success. Otherwise it will (hopefully) exit gracefully.<br />
 <<t>code>	Case successfully built.</code><br />
 
-From here on out, Tessera will ask questions about what type of information you want to get from the image. List of supported options are: <br />
-* Processes
-* Open Files and Registry Handles
-* Network Information
-* Passwords and Cryptographic Keys<br />
+From here on out, Tessera will ask questions about what type of information you want to get from the image. List of supported plugins can be found in the plugins.txt file. By default, this is the same list of plugins in the standalone copy of the Volatility Framework. To call a plugin, simply type in its name.
 
-Tessera will now guide further down into Volatility functionality and ask the user what type of information they're interested, and drill down to whatever specified granularity. Information gathered will be found in a directory located in the CWD where the image file sits in this format:<br />
+Information gathered will be found in a directory located in the CWD where the image file sits in this format:<br />
 <t><t> <code>/IMAGE_LOCATION/Tessera/data_type/drill/down/output</code>
 					 		
 
 Future Additions
 ----------------
-1. Create log of volatility commands utilized on backend
+1. Create log of volatility commands utilized on backend (FINISHED)
 2. Create base Tessera config file to store commonly chosen items (investigator names, etc.) 
 3. Generate LaTeX-built Forensic Reports of Collected Information and Commands Issued
 4. Rebuild in C++ for Speed and easier transport
--- wrapper.py
+++ wrapper.py
@@ -4,6 +4,8 @@
 # Code and current implementation of Tessera Wrapper
 # Notes: Future Implementations will probably run in C++ for speed reasons
 # Proof of concept for this is going to be done in python since that's what Volatility uses. 
+
+# In order to use this tool, volatility must be in the path as 'vol'; not vol.exe or vol.py. Make a sym link to vol.py labeled vol and add it to your path to get this tool to work. 
    
 import os
 import re
@@ -11,150 +13,206 @@ import hashlib
 from subprocess import *
 import datetime
 
-# Taken from the Volatility Website to use Vol as a Library
+def main():
+	# Start program header
+	print "====================================="
+	print " _____"
+	print "|_   _|"
+	print "  | | ___  ___ ___  ___ _ __ __ _"
+	print "  | |/ _ \/ __/ __|/ _ \ '__/ _` |"
+	print "  | |  __/\__ \__ \  __/ | | (_| |"
+	print "  \_/\___||___/___/\___|_|  \__,_|"
+	print "=====================================\n"
 
-# Start program header
-print "====================================="
-print " _____"
-print "|_   _|"
-print "  | | ___  ___ ___  ___ _ __ __ _"
-print "  | |/ _ \/ __/ __|/ _ \ '__/ _` |"
-print "  | |  __/\__ \__ \  __/ | | (_| |"
-print "  \_/\___||___/___/\___|_|  \__,_|"
-print "=====================================\n"
+	case = raw_input ('\nWould you like to make a case? (y/n) : ')
 
-case = raw_input ('\nWould you like to make a case? (y/n) : ')
+	# Getting Case Information
+	# Will be used to write a file with case information
+	if case == 'y':
+		# Name the case; similar to Autopsy
+		caseName = raw_input('Enter case name: ')
+		# Get the Investigators' names and put them on a list; May use this for report generation
+		invGtrs = []
+		name = ''
+		# Stop entering investigator names by entering a single '.' on a line
+		print 'Enter investigator names: '
+		while (name != '.'):
+			name = raw_input()
+			if (name != '.'):
+				invGtrs.append(name)
+		current = datetime.datetime.now()
+		
+		# Create a directory to store case information in
+		cwd = os.getcwd()
+		caseDir = cwd+'/'+caseName
+		# Check first to see if the directory already exists
+		if (not os.path.isdir(caseDir)):
+			os.mkdir(caseDir)
+		print 'Case will be stored in '+caseDir+'\n'
+		# Create the case info file if it doesn't already exist
+		if (not os.path.isfile(caseDir+'/case_info')):
+			touch (caseDir+'/case_info')
+	else:
+		# If no case is created, case information gets stored in CWD
+		caseDir = os.getcwd()
 
-# Getting Case Information
-if case == 'y':
-	# Name the case; similar to Autopsy
-	caseName = raw_input('Enter case name: ')
-	# Get the Investigators' names and put them on a list; May use this for report generation
-	invGtrs = []
-	name = ''
-	# Stop entering investigator names by entering a single '.' on a line
-	print 'Enter investigator names: '
-	while (name != '.'):
-		name = raw_input()
-		if (name != '.'):
-			invGtrs.append(name)
-	current = datetime.datetime.now()
-	
 	# Create an array to store the list of volatility commands used in the case
 	commands = []
 
-	# Create a directory to store case information in
-	cwd = os.getcwd()
-	caseDir = cwd+'/'+caseName
-	# Check first to see if the directory already exists
-	if (not os.path.isdir(caseDir)):
-		os.mkdir(caseDir)
-	print 'Case will be stored in '+caseDir+'\n'
-else:
-	# If no case is created, case information gets stored in CWD
-	caseDir = os.getcwd()
-	commands = []
+	# Get full path to image
+	image = raw_input('Enter full path to image: ')
 
-# Get full path to image
-image = raw_input('Enter full path to image: ')
-
-# Check if the image exists
-exist = os.path.isfile(image)
-
-# If the image can't be found, ask for a new full path
-if (exist != True):
-	image = raw_input('Image location DNE.\nEnter full path to image: ')
+	# Check if the image exists
 	exist = os.path.isfile(image)
 
-# Get the SHA-1 Hash for the image
-print '\nChecking hash of image...\n'
-f = open(image)
-L = 128
-m = hashlib.sha1()
-while L == 128:
-        block = f.read(128)
-	m.update(block)
-	L = len(block)	        
-sha = m.hexdigest()
-f.close()
+	# If the image can't be found, ask for a new full path
+	if (exist != True):
+		image = raw_input('Image location DNE.\nEnter full path to image: ')
+		exist = os.path.isfile(image)
 
-print 'SHA-1 hash is: '+sha+'\n'
+	# Get the SHA-1 Hash for the image
+	print '\nChecking hash of image...\n'
+	sha = hashit(image)
+	print 'SHA-1 hash is: '+sha+'\n'
 
-# Get the profile for the image
-print 'Processing to discover profile...'
+	# Get the profile for the image
+	print 'Processing to discover profile...'
+	profile = profile_finder(image)
 
-# Add volatility command to get profile to command list
-commands.append('vol -f '+image+' imageinfo')
-profile = Popen(['vol', '-f', image, 'imageinfo'], stdout=PIPE, stderr=PIPE)
-out, err = profile.communicate()
+	# Add volatility command to get profile to command list
+	commands.append('vol -f '+image+' imageinfo')
 
-# Make a regex to pull out the suggested profiles
-out = re.search("Suggested Profile\(s\) : .*\n", out).group(0)
-out = out[23:-1]
-profile = out.split(', ')
+	######################### TO DO: determine if the profile chosen is legit
 
-# Choose an image from either the guessed setup, or use "other" image and enter your own
-print '\nChoose an image profile: '
-i = 1
-for each in profile:
-	print str(i)+'. '+each
-	i+=1
-print str(i)+'. Other'
+	# Build the list of supported plugins from the plugins.txt list
+	plugins = pluginBuilder()
 
-# User makes Choice
-prof = raw_input('Choice: ')
+	# Choose whether or not to write results to a file
+	write = raw_input('\nWrite plugin results to file? (y/n) : ')
+	if (write == 'y'):
+		write = True
+	else:
+		write = False
 
-# Error Checking
-while (not prof.isdigit()):
-	prof = raw_input('Please choose a number: ')
-# If 'other' was selected, have the user enter their own profile. This HAS to be correct; if they goof up the syntax, it's going to throw errors later
-if (int(prof) == i):
-	profile = raw_input('Enter profile: ')
-else:
-	prof=int(prof)-1
-	profile = profile[prof]
+	# All info is loaded for case
+	print '\nCase successfully built.'
 
-######################### TO DO: determine if the profile chosen is legit
+	running = True
 
-# Build the list of supported plugins from the plugins.txt list
-cwd = os.getcwd()
-f = open(cwd+'/plugins.txt')
-plugins = f.readlines()
-plugins = [line.strip() for line in plugins]
-f.close()
+	# Now we're ready for interactive mode!
+	while (running):
+		entry = raw_input('Choose a plugin (Type exit to quit): ')
+		if (entry in plugins):
+			commands.append('vol -f '+image+' '+entry+' --profile='+profile)
+			if (write):
+				output = Popen(['vol', '-f', image, entry, '--profile='+profile], stdout=PIPE, stderr=PIPE)
+				out, err = output.communicate()
+				print out
+				f = open(caseDir+'/'+entry, 'w')
+				f.write(out)
+				f.close()
+			else: 
+				call(['vol', '-f', image, entry, '--profile='+profile])
+		if (entry == 'exit'):
+			# Write the case info file if a case was built
+			if (case == 'y'):
+				caseInfo(caseName, caseDir, current, invGtrs, sha, commands, image)
+			# Exit the program gracefully
+			running = False
+			print '\n'
 
-# Choose whether or not to write results to a file
-write = raw_input('\nWrite plugin results to file? (y/n) : ')
-if (write == 'y'):
-	write = True
-else:
-	write = False
+##################################################
+# FUNCTIONS USED IN MAIN
+##################################################
+# Touch Function: simple way to make a file (taken from a stack overflow example found here: http://stackoverflow.com/questions/1158076/implement-touch-using-python)
+def touch(fname, times=None):
+    fhandle = file(fname, 'a')
+    try:
+        os.utime(fname, times)
+    finally:
+        fhandle.close()
 
-# All info is loaded for case
-print '\nCase successfully built.'
+# Case File Write Function
+# caseName = string
+# invGtrs = array of investigator names
+# hash = sha-1 hash of file (string)
+# commands = array of commands
+# image = location of image (string)
+def caseInfo(caseName, caseDir, timestamp, invGtrs, hash, commands, image):
+	current = timestamp.strftime("%A, %B %Y %I:%M %p")
+	f = open(caseDir+'/case_info','w')
+	f.write('Case Name: '+caseName+'\n')
+	f.write('Case Directory: '+caseDir+'\n')
+	f.write('Time of Investigation: '+current+'\n')
+	f.write('Image: '+image+'\n')
+	f.write('SHA-1 Image Hash: '+hash+'\n')
+	f.write('-------------------------\nList of Investigators:\n')
+	for name in invGtrs:
+		f.write('\t'+name+'\n')
+	f.write('-------------------------\nList of volatility commands used:\n')
+	for command in commands:
+		f.write('\t'+command+'\n')
+	f.close()
+		
 
-running = True
+# SHA-1 Hash creationf unction
+# Gets the SHA-1 Hash for the image
+# Takes in a filename
+def hashit(image):
+	f = open(image)
+	L = 128
+	m = hashlib.sha1()
+	while L == 128:
+        	block = f.read(128)
+	        m.update(block)
+        	L = len(block)
+	sha = m.hexdigest()
+	f.close()
+	return sha
 
-# Now we're ready for interactive mode!
-while (running):
-	entry = raw_input('Choose a plugin (Type exit to quit): ')
-	if (entry in plugins):
-		commands.append('vol -f '+image+' '+entry+' --profile='+profile)
-		if (write):
-			output = Popen(['vol', '-f', image, entry, '--profile='+profile], stdout=PIPE, stderr=PIPE)
-			out, err = output.communicate()
-			print out
-			f = open(caseDir+'/'+entry, 'w')
-			f.write(out)
-			f.close()
-		else: 
-			call(['vol', '-f', image, entry, '--profile='+profile])
-	if (entry == 'exit'):
-		# Write the list of commands if a case was built
-		if (case == 'y'):
-			f = open(caseDir+'/command_list', 'w')
-			f.write(str(commands))
-			f.close()
-		# Exit the program gracefully
-		running = False
-	print '\n'
+# Profile Guessing function
+# uses volatility imageinfo plugin  to try to guess the profile of the image
+# Returns a string with the profile name
+def profile_finder(image):
+	# Get the profile for the image
+	profile = Popen(['vol', '-f', image, 'imageinfo'], stdout=PIPE, stderr=PIPE)
+	out, err = profile.communicate()
+
+	# Make a regex to pull out the suggested profiles
+	out = re.search("Suggested Profile\(s\) : .*\n", out).group(0)
+	out = out[23:-1]
+	profile = out.split(', ')
+
+	# Choose an image from either the guessed setup, or use "other" image and enter your own
+	print '\nChoose an image profile: '
+	i = 1
+	for each in profile:
+		print str(i)+'. '+each
+		i+=1
+	print str(i)+'. Other'
+
+	# User makes Choice
+	prof = raw_input('Choice: ')
+
+	# Error Checking
+	while (not prof.isdigit()):
+		prof = raw_input('Please choose a number: ')
+	# If 'other' was selected, have the user enter their own profile. This HAS to be correct; if they goof up the syntax, it's going to throw errors later
+	if (int(prof) == i):
+		profile = raw_input('Enter profile: ')
+		return profile
+	else:
+		prof=int(prof)-1
+		profile = profile[prof]
+		return profile
+
+def pluginBuilder():
+	cwd = os.getcwd()
+	f = open(cwd+'/plugins.txt')
+	plugins = f.readlines()
+	plugins = [line.strip() for line in plugins]
+	f.close()
+	return plugins
+
+# Calling Main Function
+main()
